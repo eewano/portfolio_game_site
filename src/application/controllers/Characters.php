457934f318388d2a77807d76c969e8a2361111dc
eewano <eewano@gmail.com>
2020-03-20 16:59:24 +0900

    キャラクター新規登録機能を追加。

--- src/application/controllers/Characters.php
+++ src/application/controllers/Characters.php
@@ -32,4 +32,35 @@ class Characters extends CI_Controller {
         $this->load->view('characters/chara_detail', $data);
         $this->load->view('templates/footer');
     }
+
+    public function chara_register()
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('name', 'キャラクター名', 'required');
+        $this->form_validation->set_rules('sex', '性別', 'required');
+        $this->form_validation->set_rules('class', 'ユニット', 'required');
+        $this->form_validation->set_rules('hp', 'HPの成長率', 'required');
+        $this->form_validation->set_rules('strength', '力の成長率', 'required');
+        $this->form_validation->set_rules('magic', '魔力の成長率', 'required');
+        $this->form_validation->set_rules('dexterity', '技の成長率', 'required');
+        $this->form_validation->set_rules('speed', '素早さの成長率', 'required');
+        $this->form_validation->set_rules('luck', '運の成長率', 'required');
+        $this->form_validation->set_rules('defense', '守備力の成長率', 'required');
+        $this->form_validation->set_rules('resistance', '魔法防御の成長率', 'required');
+        $this->form_validation->set_rules('skill01', 'スキル01', 'required');
+        $this->form_validation->set_rules('skill02', 'スキル02', 'required');
+        $this->form_validation->set_rules('profile', 'プロフィール', 'required');
+        $this->form_validation->set_rules('message', '一言', 'required');
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('templates/header');
+            $this->load->view('characters/chara_register');
+            $this->load->view('templates/footer');
+        } else {
+            $this->characters_model->set_chara_data();
+            $this->load->view('news/success');
+        }
+    }
 }
--- src/application/models/Characters_model.php
+++ src/application/models/Characters_model.php
@@ -17,4 +17,41 @@ class Characters_model extends CI_Model {
         $query = $this->db->get_where('character', array('id' => $id));
         return $query->row_array();
     }
+
+    public function set_chara_data()
+    {
+        $data = array(
+            'name' => $this->input->post('name'),
+            // 'icon' => $this->input->post('icon'),
+            // 'photo01' => $this->input->post('photo01'),
+            // 'photo02' => $this->input->post('photo02'),
+            // 'photo03' => $this->input->post('photo03'),
+            'sex' => $this->input->post('sex'),
+            'class' => $this->input->post('class'),
+            // 'blood01' => $this->input->post('blood01'),
+            // 'blood02' => $this->input->post('blood02'),
+            // 'blood03' => $this->input->post('blood03'),
+            // 'blood04' => $this->input->post('blood04'),
+            'hp' => $this->input->post('hp'),
+            'strength' => $this->input->post('strength'),
+            'magic' => $this->input->post('magic'),
+            'dexterity' => $this->input->post('dexterity'),
+            'speed' => $this->input->post('speed'),
+            'luck' => $this->input->post('luck'),
+            'defense' => $this->input->post('defense'),
+            'resistance' => $this->input->post('resistance'),
+            'skill01' => $this->input->post('skill01'),
+            'skill02' => $this->input->post('skill02'),
+            // 'skill03' => $this->input->post('skill03'),
+            // 'skill04' => $this->input->post('skill04'),
+            // 'skill05' => $this->input->post('skill05'),
+            // 'skill06' => $this->input->post('skill06'),
+            // 'skill07' => $this->input->post('skill07'),
+            // 'skill08' => $this->input->post('skill08'),
+            'profile' => $this->input->post('profile'),
+            'message' => $this->input->post('message')
+        );
+
+        return $this->db->insert('character', $data);
+    }
 }
