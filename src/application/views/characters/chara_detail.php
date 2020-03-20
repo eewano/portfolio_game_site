a300eb1c5a153f9205ccfbd4fa400a2a13c87aa3
eewano <eewano@gmail.com>
2020-03-20 15:09:20 +0900

    キャラクター紹介ページのviewを新規作成。

---
+++ src/application/views/characters/chara_detail.php
@@ -0,0 +1,16 @@
+<main>
+    <h3>Fire Emblem 聖戦の系譜</h3>
+    <h1><?php echo $chara['name']; ?></h1>
+    <p>性別 <?php echo $chara['sex']; ?></p>
+    <p>ユニット <?php echo $chara['class']; ?></p>
+    <p>HP <?php echo $chara['hp']; ?></p>
+    <p>力 <?php echo $chara['strength']; ?></p>
+    <p>魔力 <?php echo $chara['magic']; ?></p>
+    <p>技 <?php echo $chara['dexterity']; ?></p>
+    <p>素早さ <?php echo $chara['speed']; ?></p>
+    <p>運 <?php echo $chara['luck']; ?></p>
+    <p>守備力 <?php echo $chara['defense']; ?></p>
+    <p>魔法防御 <?php echo $chara['resistance']; ?></p>
+    <p>プロフィール <?php echo $chara['profile']; ?></p>
+    <p>一言 <?php echo $chara['message']; ?></p>
+</main>
